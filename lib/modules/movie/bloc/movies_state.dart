20997f2c92098bfc634eddf0ba6c9105bd737007
Ali Tanrıverdi <alitanriverdi@outlook.com>
2021-09-26 23:02:45 +0300

    unnecessary files are deleted.

--- lib/modules/movie/bloc/movies_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'movies_bloc.dart';
-
-abstract class MoviesState {}
-
-class LoadingMovies extends MoviesState {}
-
-class FailedMovies extends MoviesState {}
-
-class LoadedMovies extends MoviesState {}
